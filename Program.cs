04cb85f74c8f2f8afee61c2bc30d15a5e98b899c
Victor <victor.melocariati@gmail.com>
2020-11-18 16:29:45 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Dados_cadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // Faça um programa que leia e valide as seguintes informações:
+
+            // 1. Nome: diferente de vazio;
+            // 2. Idade: entre 0 e 150;
+            // 3. Salário: maior que zero;
+            // 4. Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a));
+            string undefined = null;
+            Console.WriteLine("Olá!\nSeja Bem-vindo(a)");
+            Console.WriteLine("  ");
+            Console.WriteLine("FAÇA SEU CADASTRO");
+            Console.WriteLine("  ");
+
+            //NOME
+            Console.Write("Insira seu nome: ");
+            string nome = Console.ReadLine();
+            undefined = Console.ReadLine();
+            
+            while(nome == undefined){
+                Console.Write("Por favor, insira seu nome: ");
+                nome = Console.ReadLine();
+                undefined = Console.ReadLine();
+            }
+
+            //IDADE
+            Console.Write("Qual a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+            while(idade > 150){
+                Console.Write("Insira uma idade válida: ");
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            //SALÁRIO
+            Console.Write("Qual o seu salário? ");
+            int salario = int.Parse(Console.ReadLine());
+            while(salario < 1){
+                Console.Write("Insira um valor válido: ");
+                salario = int.Parse(Console.ReadLine());
+            }
+
+            //ESTADO CIVIL
+            // string S, C, V, D;
+            Console.WriteLine("Qual o seu Estado Civil?\nESCOLHA UMA DAS OPÇÕES ABAIXO: \nS - (Solteiro(a)\nC - (Casado(a)\nV - (Viúvo(a))\nD - (Divorciado(a)");
+            string estado = Console.ReadLine();
+            // while(estado == S; C; V; D){
+                
+            // }
+        }
+    }
+}
